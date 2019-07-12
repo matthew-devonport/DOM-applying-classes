e5e52d415f4437cf0c2074362c5b1f892c682332
matthew-devonport <roarsintheeast@gmail.com>
2019-07-13 01:08:07 +1200

    DONE

--- classes.js
+++ classes.js
@@ -4,22 +4,29 @@
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
-  // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
-  // Your turn! Create a new function called `two`, then call it from here.
+one()
+two()
+three()
+makeVisible()   
+
 }
 
 function one () {
-  // First, we have to find the element:
-  var one = document.getElementById('one')
-
-  // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+ var one = document.getElementById('one')
+ one.classList.add('blue')
 }
 
-// CREATE FUNCTION two HERE
+function two () {
+ var two = document.getElementById('two')
+ two.classList.add('green')
+}
 
-// CREATE FUNCTION three HERE
+function three () {
+ var three = document.getElementById('three')
+ three.classList.add('red')
+}
 
-// CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+ var makeVisible = document.getElementsByClassName('invisible')[0]
+ makeVisible.classList.add('visible')
+}
